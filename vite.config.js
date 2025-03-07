99d1b18f3812efe47531ce3b2d179e55f13f6736
Denny Soemantri <sidennsoe@gmail.com>
2025-03-07 16:14:56 +0700

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/sisnekas/',  // Tambahkan ini, sesuaikan dengan nama repo
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
